6be9c4036995fe0b488196d82ecb46f71f51315c
kmkinney <65429603+kmkinney@users.noreply.github.com>
2021-01-11 20:56:46 -0600

    More testing

---
+++ Test/Car.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Car.h"
+using namespace std;
+
+void Car::printYear() const{
+    cout << this->year << " year car." << endl;
+}
--- Test/main.cpp
+++ Test/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "Car.h"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     cout << "Hi there" << endl;
+    Car def;
+    Car *newer = new Car(2020);
+    def.printYear();
+    newer->printYear(); 
 }
