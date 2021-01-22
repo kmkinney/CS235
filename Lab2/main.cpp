87eddbd02ac8b6cfd045721910a504061b559240
kmkinney <65429603+kmkinney@users.noreply.github.com>
2021-01-22 16:59:15 +0000

    Making lab2 folder

---
+++ Lab2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void ReadToSet(string _fileName);
+void ReadToVector(string _fileName);
+
+int main(int argc, char *argv[])
+{
+    if(argc>1)
+    {
+
+    }
+    return 0;
+}
