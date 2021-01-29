e943df7c853cf72663df9ba881b9daa5c952eb74
kmkinney <65429603+kmkinney@users.noreply.github.com>
2021-01-29 14:57:52 -0700

    more notes

--- Unit3/Containers/Maps.h
+++ Unit3/Containers/Maps.h
@@ -2,7 +2,39 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <unordered_map>
 using namespace std;
+
+template<typename ItemType>
+void print(ItemType items) {
+
+ std::cout << '[';
+
+ for (typename ItemType::iterator i = items.begin(); i != items.end(); i++) {
+   if (i != items.begin())
+     std::cout << ", ";
+   std::cout << '\'' << *i << '\'';
+ }
+
+ std::cout << ']' << std::endl;
+
+}
+
+
+template<typename K, typename V>
+void print(std::map<K,V> items) {
+
+ std::cout << '{';
+
+ for (typename std::map<K,V>::iterator i = items.begin(); i != items.end(); i++) {
+   if (i != items.begin())
+     std::cout << ", ";
+   std::cout << i->first << '=' << i->second;
+ }
+
+ std::cout << '}' << std::endl;
+
+}
 
 /*
     Stores <key, value> pairs
@@ -10,9 +42,19 @@ using namespace std;
     functions
     insert(key_type key, value_type value)
     erase(key_type key)
-    at(key_type key)
+    at(key_type key) also []
     empty()
     size()
+
+    C++STL Map
+    pair<iterator,bool> insert(pair<key_type, value_type>(key, value));
+    int erase (key_type key);
+    value_type at(key_type key);
+    my_map[key_type key];
+    iterator find(key_type key);
+    bool empty();
+    int size();
+    void clear();
 */
 void MapTest()
 {
@@ -28,4 +70,11 @@ void MapTest()
 
     names["gretchen"] = "kinney";
     cout << names["gretchen"] << endl;
+
+    //names.insert("aiden", "kinney"); doesn't work
+    names.insert(pair<string, string>("aiden", "kinney"));
+
+    print(names);
+
+    unordered_map<string, int> no_order;
 }
