cd18d51870d7acef9be89430d7d61dcc3021dfab
kmkinney <65429603+kmkinney@users.noreply.github.com>
2021-02-02 12:48:03 -0700

    Finished lab 2

--- Lab2/final/text-gen.cpp
+++ Lab2/final/text-gen.cpp
@@ -14,8 +14,9 @@ void ReadWordsToVector(vector<string> &words, string file_name);
 void MakePhraseMap(map<list<string>, vector<string>> &phrase_map, vector<string> &words);
 void GenerateTextFromPhraseMap(map<list<string>, vector<string>> &phrase_map, vector<string> &words);
 
-const int M = 4;
-const int NUM_WORDS = 200;
+const int M = 2;
+const int NUM_WORDS = 250;
+const int LINE_SIZE = 15;
 
 int main(int argc, char *argv[])
 {
@@ -32,7 +33,6 @@ int main(int argc, char *argv[])
             ReadWordsToVector(words, file_name);
         }
 
-        
         map<list<string>, vector<string>> phrase_map;
         MakePhraseMap(phrase_map, words);
 
@@ -62,35 +62,60 @@ void GenerateTextFromPhraseMap(map<list<string>, vector<string>> &phrase_map, ve
 {
     srand(time(NULL));
     list<string> state;
-    state.clear();
     //added random start
-    int rand_start = rand() % (words.size() - M);
+    int rand_ind = rand() % (words.size() - M);
     for(int i = 0; i < M; i++)
     {
-        state.push_back(words[rand_start]);
-        rand_start++;
+        state.push_back(words[rand_ind]);
+        rand_ind++;
     }
     ofstream outF("output.txt");
+    string line;
+    int line_count = 0;
+    bool newSentence = true;
     for(int i = 0; i < NUM_WORDS; i++)
     {
+        string curr;
         if(phrase_map[state].size() > 0)
         {
             int ind = rand() % phrase_map[state].size();
-            outF << phrase_map[state][ind];
-            if(phrase_map[state][ind].compare("\n") != 0)
-                outF << " ";
-            state.push_back(phrase_map[state][ind]);
+            curr = phrase_map[state][ind];
+            line_count++;
+            if(!line.empty() && (curr.compare(".") == 0 | curr.compare(",") == 0)){
+                line = line.substr(0, line.length() - 1) + curr + " ";
+                if(curr.compare(".") == 0)
+                    newSentence = true;
+            }
+            else if(newSentence){
+                line += toupper(curr.at(0));
+                if(curr.length() > 1)
+                    line += curr.substr(1) + " ";
+                newSentence = false;
+            }
+            else{
+                line += curr + " ";
+            }
+
+            if(line_count == LINE_SIZE){
+                line_count = 0;
+                outF << line << endl;
+                line = "";
+            }
+            state.push_back(curr);
             state.pop_front();
         }
         else
         {
             state.clear();
+            rand_ind = rand() % (words.size() - M);
             for(int i = 0; i < M; i++)
             {
-                state.push_back("");
+                state.push_back(words[rand_ind]);
+                rand_ind++;
             }
         }
     }
+    outF << line;
     outF.close();
 }
 
@@ -105,15 +130,21 @@ void ReadWordsToVector(vector<string> &words, string file_name)
         while(ss >> token)
         {
             string nopunct = "";
+            string punct = "";
             for(auto &c:token){
                 //include punctuation at end of words
-                if(isalpha(c) | c == '.' | c == ',' | c == '\"' | c == '\''){
+                if(isalpha(c)){
                     nopunct += c;
                 }
+                else if(c == '.' | c == ',' ){
+                    punct += c;
+                }
             }
-            words.push_back(nopunct);
+            if(!nopunct.empty())
+                words.push_back(nopunct);
+            if(!punct.empty())
+                words.push_back(punct);
         }
-        words.push_back("\n");
     }
     inF.close();
 }
